1df53f771f2a9bd9fac7990ff0a00eeba37156f4
Pedro Coutinho <pedro_coutinho18@outlook.com>
2023-03-17 10:53:37 +0000

    Complete ChangeString exercise

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,7 +6,20 @@ namespace ChangeString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Give me a string");
+            string inputString = Console.ReadLine();
+            Console.WriteLine("\nGive me a character");
+            char inputChar;
+            char.TryParse(Console.ReadLine(), out inputChar);
+
+            string newString = "";
+            foreach (char c in inputString)
+            {
+                if (c != inputChar)
+                    newString = newString + c;
+            }
+
+            Console.WriteLine(newString);
         }
     }
 }
